70f129c59447d1f45eaa5550dceb2d31bb54b2f9
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2016-09-14 07:57:02 +1000

    Initial commit.

---
+++ IFAGoogleMobileAdsSupport/IFAGoogleMobileAdsSupport/IFAGoogleMobileAdsSupport.h
@@ -0,0 +1,19 @@
+//
+//  IFAGoogleMobileAdsSupport.h
+//  IFAGoogleMobileAdsSupport
+//
+//  Created by Marcelo Schroeder on 14/09/2016.
+//  Copyright © 2016 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IFAGoogleMobileAdsSupport.
+FOUNDATION_EXPORT double IFAGoogleMobileAdsSupportVersionNumber;
+
+//! Project version string for IFAGoogleMobileAdsSupport.
+FOUNDATION_EXPORT const unsigned char IFAGoogleMobileAdsSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFAGoogleMobileAdsSupport/PublicHeader.h>
+
+
